6da10c0b4dc0fb79f7138783513aba801e6a024f
Anton <azarapinnik@gmail.com>
2023-01-10 15:18:28 +0200

    create api service

---
+++ src/services/MarverService.js
@@ -0,0 +1,26 @@
+
+
+class MarverService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=f2c13ab6f69535c61a86e989c97e34cb';
+
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResourse(`${this._apiBase}characters/${id}&${this._apiKey}`);
+    }
+}
+
+export default MarverService;
