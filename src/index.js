cfa9945d69afd06fdf76eca8e2f78f2a42daaca4
Anton <azarapinnik@gmail.com>
2023-01-12 14:55:09 +0200

    get a random char

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarverService from './services/MarverService';
 
 import './style/style.scss';
-
-const marverService = new MarverService();
-
-marverService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
